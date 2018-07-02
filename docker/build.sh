96594fe55a8c30d6fe3d56c90e0db979aa2d2849
Angel Olea <angel@lexigram.io>
2018-07-02 11:50:48 +0200

    Build script to use azure docker registry

---
+++ docker/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+LXG_DOCKER_PROJECT="postgrest:v0.4.4.0"
+
+if [ -z ${LXG_DOCKER_REGISTRY} ]; then
+    echo 'You must define the environment variable LXG_DOCKER_REGISTRY'
+    exit 1
+fi
+
+docker build -t ${LXG_DOCKER_REGISTRY}.azurecr.io/${LXG_DOCKER_PROJECT} .
+
+az acr login --name ${LXG_DOCKER_REGISTRY}
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+docker push ${LXG_DOCKER_REGISTRY}.azurecr.io/${LXG_DOCKER_PROJECT}
